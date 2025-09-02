03809777e1e37f5eb35d94a7270869ab0699f384
ateebqaiser <ateebqaiser243@gmail.com>
2025-09-02 22:22:39 +0500

    constants & literals Added

---
+++ Cpp_Basics/Constants&Literals.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+main()
+{
+//	Constant are those which value cannot change in program
+	const int a=20;
+	cout<<"Value of a : "<<a<<endl;
+	
+//	a=100; Error we cannot modifay value of a because it is constant
+	
+//	Literals are those values that directly give to variable;
+	int age=20;  //Integer Literal
+	float height=5.8; //Float Literal
+	char gender ='M'; //Gender Literal
+	string name = "Ateeb"; //String Literal
+	bool isTrue = //Boolean Literal
+}
