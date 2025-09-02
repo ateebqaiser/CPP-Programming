b29bac811d574381a50aa46dabb0d2a0c7b9828c
ateebqaiser <ateebqaiser243@gmail.com>
2025-09-02 22:01:00 +0500

    cin & cout examples Added

---
+++ Cpp_Basics/Input&Output.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int a=10;
+	int b;
+//	For output use cout :
+	cout<<"Hello Ateeb"<<endl;
+	cout<<"Value of a : "<<a<<endl;
+	
+//	For output use cin :
+	cout<<"Enter value of b : ";
+	cin>>b;
+	cout<<"Value of b : "<<b;
+
+}
