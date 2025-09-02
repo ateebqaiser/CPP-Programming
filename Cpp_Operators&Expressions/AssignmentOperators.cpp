39f2dd496645948e2ea8f29c234a8f2e22e70e37
ateebqaiser <ateebqaiser243@gmail.com>
2025-09-03 01:29:22 +0500

    assignment operators Added

---
+++ Cpp_Operators&Expressions/AssignmentOperators.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+main()
+{
+//Assignment operator : "="
+	int n=20;
+	cout<<"Value of n : "<<n<<endl;
+	
+//Compound Assignment operators : "+=,-=,*=,/=,%="
+	n+=10;
+	cout<<"Value of n : "<<n<<endl;
+	n-=5;
+	cout<<"Value of n : "<<n<<endl;
+	n*=8;
+	cout<<"Value of n : "<<n<<endl;
+	n/=10;
+	cout<<"Value of n : "<<n<<endl;
+	n%=10;
+	
+	
+}
