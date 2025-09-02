88b5f9bc670fcad04bc67745e524affe7469d853
ateebqaiser <ateebqaiser243@gmail.com>
2025-09-03 01:10:16 +0500

    relational operators Added

---
+++ Cpp_Operators&Expressions/RelationalOperators.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+main()
+{
+//Relational operators : "==,!=,>,>=,<,<="
+	int number1=20,number2=30;
+	
+	cout<<number1<<" = "<<number2<<" : "<<(number1==number2)<<endl;
+	cout<<number1<<" != "<<number2<<" : "<<(number1!=number2)<<endl;
+	cout<<number1<<" > "<<number2<<" : "<<(number1>number2)<<endl;
+	cout<<number1<<" >= "<<number2<<" : "<<(number1>=number2)<<endl;
+	cout<<number1<<" < "<<number2<<" : "<<(number1<number2)<<endl;
+	cout<<number1<<" <= "<<number2<<" : "<<(number1<=number2)<<endl;
+}
