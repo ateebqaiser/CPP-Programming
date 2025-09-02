1483027bbeb8ae3d141de0042a1339e676a540ba
ateebqaiser <ateebqaiser243@gmail.com>
2025-09-02 22:27:19 +0500

    C++ comments Added

---
+++ Cpp_Basics/Comments.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main()
+{ 
+
+//Single line Comments :
+			//This program is about comments.
+			
+//Multi-line Comments : 
+			/*Hello
+			great great peeps*/		
+			
+}
