2a3539ca2ad3ae8ae7256ff0324e324202872a66
ateebqaiser <ateebqaiser243@gmail.com>
2025-09-02 22:10:59 +0500

    variables & data types Added

---
+++ Cpp_Basics/Variables&Data Types.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+using namespace std;
+main()
+{
+//	Data type:
+	//	Integer : int
+	//	Real numbers : float
+	//	Character : char
+	//	String : string
+	//	Boolean : bool   		
+	
+	int age = 21;
+	float height = 5.8;
+	char gender = 'M';
+	string name = "Ateeb qaiser";
+	bool isPass = true;
+	
+	cout<<"Name of student : "<<name<<endl;
+	cout<<"Age of student : "<<age<<endl;
+	cout<<"Height of student : "<<height<<endl;
+	cout<<"Gender of student : "<<gender<<endl;
+	cout<<"Is student pass : "<<isPass<<endl;
+	
+	
+}
