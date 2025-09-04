1e872a1a3ec612006df94c8100edd169c7f79f5c
ateebqaiser <ateebqaiser243@gmail.com>
2025-09-04 05:41:31 +0500

    Added program to check positive, negative or zero using if statements

---
+++ Cpp_ControlStatements/Decision-Making-Statements/if-statement/check-number-if.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int num;
+	cout<<"Enter number : ";
+	cin>>num;
+	if(num>0)
+	{
+		cout<<"Number is positive";
+	}
+	if(num<0)
+	{
+		cout<<"Number is negative";
+	}
+	if(num==0)
+	{
+		cout<<"Number is zero";
+	}
+}
