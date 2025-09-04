38ddfc9834c55625e444216dca819f402570e73d
ateebqaiser <ateebqaiser243@gmail.com>
2025-09-04 06:05:48 +0500

    Added program to check even or odd number using if-else

---
+++ Cpp_ControlStatements/Decision-Making-Statements/if-else statement/even-odd-if-else.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int num;
+    cout<<"Enter number : ";
+    cin>>num;
+    if(num%2==0){
+    	cout<<"Number "<<num<<" is even";
+	}
+	else{
+		cout<<"Number "<<num<<" is odd";
+	}
+}
+
