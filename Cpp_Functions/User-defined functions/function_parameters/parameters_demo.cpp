c1f8f227e5782435b6392c250e03d758e9a9a1c4
ateebqaiser <ateebqaiser243@gmail.com>
2025-09-07 10:04:37 +0500

    Add program demonstrating function parameters

---
+++ Cpp_Functions/User-defined functions/function_parameters/parameters_demo.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+void data(int age,string name,char grade,float height){
+	cout<<"Name : "<<name<<endl;
+	cout<<"Age : "<<age<<endl;
+	cout<<"Grade : "<<grade<<endl;
+	cout<<"Height : "<<height<<endl;
+}
+
+main()
+{
+	data(21,"Ateeb",'A',5.8);
+}
