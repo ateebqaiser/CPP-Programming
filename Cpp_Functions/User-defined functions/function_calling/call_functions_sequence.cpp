4610c3699a916a390a24fe9a500ceed018919469
ateebqaiser <ateebqaiser243@gmail.com>
2025-09-07 09:48:47 +0500

    Add program demonstrating sequence of function calls

---
+++ Cpp_Functions/User-defined functions/function_calling/call_functions_sequence.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+void greet(){
+	cout<<"Hello this is ateeb"<<endl;
+}
+
+void bye(){
+	cout<<"Bye! Have a nice day."<<endl;
+}
+
+main()
+{
+	greet();
+	cout<<"Welcome to my program!"<<endl;
+	bye();
+}
